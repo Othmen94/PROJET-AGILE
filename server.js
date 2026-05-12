a9aaf5767e8b663bf48ef5e24d6eba8664fd7721
othmen <othmane.bedwi@gmail.com>
2026-05-12 11:05:54 +0200

    aaaa

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const fs = require('fs');
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static('.'));
 app.use('/uploads', express.static('uploads'));
 
 const JWT_SECRET = 'postop_secret_key_2026';
